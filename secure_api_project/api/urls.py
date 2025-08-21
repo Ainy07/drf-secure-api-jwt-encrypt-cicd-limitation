84bf88089c7883d904b21c1428e3e3df90608cdd
Ainy Gupta <111982864+Ainy07@users.noreply.github.com>
2025-08-21 15:48:47 +0530

    Update urls.py

--- secure_api_project/api/urls.py
+++ secure_api_project/api/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import SecureDataListCreate
+def api_home(request):
+    return JsonResponse({"message": "API root is working 🚀", "available_endpoints": ["/secure-data/", "/token/", "/token/refresh/"]})
 
 urlpatterns = [
+    path('', api_home),  
     path('secure-data/', SecureDataListCreate.as_view(), name="secure-data"),
 ]
